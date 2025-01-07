2c82b13ee9aa72b2cb790373c7d1c66666cd9527
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-01-07 17:33:46 +0700

    Add states such as score and typeOfVerb to use for score system

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,30 +3,34 @@ import ChoiceButton from './ChoiceButton'
 import { verbs } from '../data/verbs'
 
 export default function Game() {
+  const [score, setScore] = useState(0)
   const [verb, setVerb] = useState('decide')
+  const [typeOfVerb, setTypeOfVerb] = useState('Infinitive') // 0 for gerund and 1 for infinitive type
 
   const randomWord = () => {
     const type = Math.floor(Math.random() * 2)
     const index = Math.floor(Math.random() * verbs[type].length)
 
     setVerb(verbs[type][index])
+    setTypeOfVerb(type === 0 ? 'Gerund' : 'Infinitive')
   }
 
   return (
     <div className="container">
       <h2 className="header">Gerund or Infinitive</h2>
-      <>
-        <p className="wordBox">{verb}</p>
-        <div className="buttonBox">
-          {['Gerund', 'Infinitive', 'Both'].map((choice) => (
-            <ChoiceButton
-              key={choice}
-              choice={choice}
-              randomWord={randomWord}
-            />
-          ))}
-        </div>
-      </>
+      <p className="wordBox">{verb}</p>
+      <p className="scoreBoard">Score: {score}</p>
+      <div className="buttonBox">
+        {['Gerund', 'Infinitive', 'Both'].map((choice) => (
+          <ChoiceButton
+            key={choice}
+            choice={choice}
+            randomWord={randomWord}
+            setScore={setScore}
+            typeOfVerb={typeOfVerb}
+          />
+        ))}
+      </div>
     </div>
   )
 }
